81554d0e3688c74c9efb326a614e1f925db56d1b
Sachith Gullapalli <sachith96@gmail.com>
2014-08-10 22:29:56 -0400

    refactored reimbursement creation

--- app/models/reimbursement.rb
+++ app/models/reimbursement.rb
@@ -1,4 +1,44 @@
 class Reimbursement < ActiveRecord::Base
 	belongs_to :user
 	validates :user_id, presence: true
+	def self.create(file, user_id, tournament, expense_type, details, amount)
+		if file!=nil
+		  	@reimbursement = Reimbursement.new
+		  	@reimbursement.user_id=user_id
+		  	@reimbursement.tournament=tournament
+		
+		  	@reimbursement.expense_type=expense_type
+		  	@reimbursement.details=details
+		  	@reimbursement.status=0
+		  	ext=""
+		  	if file.headers.include? "Content-Type: image/jpeg"
+		  		ext=".jpg"
+		  	elsif file.headers.include? "Content-Type: image/jpeg"
+		     	ext=".gif"
+		  	elsif file.headers.include? "Content-Type: image/pjpeg"
+		  		ext=".jpg"
+		  	elsif file.headers.include? "Content-Type: image/png"
+		  		ext=".png"
+		  	end	
+		  	newname=SecureRandom.uuid+ext
+		  	s3 = AWS::S3.new
+		  	img=s3.buckets['ydareimbursements'].objects[newname].write(file.tempfile.read)
+		  	@reimbursement.receipt=img.public_url.to_s
+		  	@reimbursement.date_submitted=Time.new
+		  	dollars=0
+		  	cents=0
+		  	dollarsmatch=/\$?([0-9]+)\.?/.match(amount)
+		  	if dollarsmatch != nil
+		  		dollars = dollarsmatch[1].to_i
+		  	end
+		  	centsmatch=/\.([0-9]+)/.match(amount)
+		  	if centsmatch != nil
+		  		cents = centsmatch[1].to_i
+		  	end
+		  	@reimbursement.amount=dollars*100+cents
+		  	@reimbursement.save
+		end
+	end
+
+
 end
